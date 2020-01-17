21a51ab8322d6e9118b9f835d336038822c5870d
oo1463 <oooo1463@naver.com>
2020-01-17 14:57:46 +0900

    other url

--- KU_Board/board/urls.py
+++ KU_Board/board/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.board, name='board'),
+    path('elements/', views.elements, name='elements'),
+    path('generic/', views.generic, name='generic'),
+
 ]
--- KU_Board/board/views.py
+++ KU_Board/board/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.template import loader
 # Create your views here.
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+
 
 def board(request):
     #template = loader.get_template('board/index.html')
@@ -9,5 +11,11 @@ def board(request):
     context = {
         'current_date': now
     }
-
     return render(request, 'board/otherhtml/index.html', context)
+
+def generic(request):
+    return render(request, 'board/otherhtml/generic.html')
+
+@csrf_exempt
+def elements(request):
+    return render(request, 'board/otherhtml/elements.html')
