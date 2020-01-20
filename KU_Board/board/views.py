22e1d231896f14620429a21d264c2f88366a1151
oo1463 <oooo1463@naver.com>
2020-01-20 18:49:15 +0900

    bootstrap learing

--- KU_Board/board/views.py
+++ KU_Board/board/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 # Create your views here.
 from datetime import datetime
-from django.views.decorators.csrf import csrf_exempt
+
 
 
 def board(request):
@@ -11,11 +11,4 @@ def board(request):
     context = {
         'current_date': now
     }
-    return render(request, 'board/otherhtml/index.html', context)
-
-def generic(request):
-    return render(request, 'board/otherhtml/generic.html')
-
-@csrf_exempt
-def elements(request):
-    return render(request, 'board/otherhtml/elements.html')
+    return render(request, 'board/board_index.html', context)
