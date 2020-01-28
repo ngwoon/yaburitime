544891d5fecd3cae691268f0f3d39e99e225288d
oo1463 <oooo1463@naver.com>
2020-01-28 23:28:11 +0900

    페이지네이션

--- KU_Board/board/views.py
+++ KU_Board/board/views.py
@@ -7,14 +7,31 @@ from .models import Post
 from .forms import Postform
 from django.utils import timezone
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 
 def board(request):
     #template = loader.get_template('board/index.html')
-    board_post = Post.objects.all()
+    board_post = Post.objects.all().order_by('-boardNum')
+
+    posts_of_page = 3 # 한 페이지당 나타낼 글의 개수
+    paginator = Paginator(board_post, posts_of_page)
+    page = request.GET.get('page')
+    pageposts = paginator.get_page(page)
+    prepage = paginator.get_page(page)
+    nextpage = paginator.get_page(page)
+
+    if pageposts.number > 1:
+        prepage = paginator.page(pageposts.number-1)
+    if pageposts.number < pageposts.paginator.num_pages:
+        nextpage =paginator.page(pageposts.number+1)
+
     context = {
         'board_post':board_post,
+        'pageposts':pageposts,
+        'prepage':prepage,
+        'nextpage':nextpage,
     }
     return render(request, 'board/board_index.html', context)
 
@@ -30,7 +47,7 @@ def writingpost(request):
             post.save()
             return redirect('/board/', pk=post.pk)
         else:
-            messages.info(request, '제목 또는 내용이 입력되지 않았습니다.')
+            messages.error(request, '제목 또는 내용이 입력되지 않았습니다.')
     else:
         form = Postform()
     return render(request, 'board/posting.html', {'form': form})
@@ -41,18 +58,20 @@ def postdetail(request,pk):
     context = {
         'board_post': board_post,
     }
+    board_post.count_up()
     return render(request, 'board/post_detail.html', context)
 
 def recommend(request,pk):
    post = Post.objects.get(pk=pk)
    post.recommend += 1
+   post.Field -= 1
    post.save()
-
    return postdetail(request,pk)
 
 def unrecommend(request,pk):
    post = Post.objects.get(pk=pk)
    post.unrecommend += 1
+   post.Field -= 1
    post.save()
 
    return postdetail(request,pk)
