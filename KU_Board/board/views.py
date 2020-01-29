269dbf28f4487f2f3873abe7b653b48600e7e448
oo1463 <oooo1463@naver.com>
2020-01-29 14:55:46 +0900

    보드 완성

--- KU_Board/board/views.py
+++ KU_Board/board/views.py
@@ -12,26 +12,30 @@ from django.core.paginator import Paginator
 
 
 def board(request):
-    #template = loader.get_template('board/index.html')
     board_post = Post.objects.all().order_by('-boardNum')
 
-    posts_of_page = 3 # 한 페이지당 나타낼 글의 개수
+    posts_of_page = 5 # 한 페이지당 나타낼 글의 개수
     paginator = Paginator(board_post, posts_of_page)
     page = request.GET.get('page')
     pageposts = paginator.get_page(page)
     prepage = paginator.get_page(page)
     nextpage = paginator.get_page(page)
+    is_lastpage = False
 
     if pageposts.number > 1:
         prepage = paginator.page(pageposts.number-1)
     if pageposts.number < pageposts.paginator.num_pages:
         nextpage =paginator.page(pageposts.number+1)
 
+    if pageposts.number+2 >= pageposts.paginator.num_pages:
+            is_lastpage = True
+
     context = {
         'board_post':board_post,
         'pageposts':pageposts,
         'prepage':prepage,
         'nextpage':nextpage,
+        'is_lastpage':is_lastpage,
     }
     return render(request, 'board/board_index.html', context)
 
