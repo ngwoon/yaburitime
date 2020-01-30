5950e2fdc8b50bd5df79bf726531f6d2dffa1658
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2020-01-30 20:44:21 +0900

    Update mypage urls.py
    mypage에 urls.py가 비워둬서 서버 실행이 안된 부분을 해결

--- venv/Scripts/pip3-script.py
+++ venv/Scripts/pip3-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\todor\PycharmProjects\KU_Board\venv\Scripts\python.exe
+#!C:\Users\sys09\PycharmProjects\KU_Board\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
 __requires__ = 'pip==19.0.3'
 import re
