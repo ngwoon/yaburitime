06f3120026e2d64e0fea2b6cbec5b23ab7e36cbf
oo1463 <oooo1463@naver.com>
2020-02-01 18:02:00 +0900

    merged with master

--- KU_Board/KU_Board/views.py
+++ KU_Board/KU_Board/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import TemplateView
 
+
 # 로그인 세션에 따라 template_name이 index.html 혹은 board_index.html로 이동
 class HomeView(TemplateView):
     template_name = 'index.html'
-    
--- KU_Board/account/models.py
+++ KU_Board/account/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinLengthValidator
 # from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
 class User(models.Model):
     id = models.CharField(max_length=15, primary_key=True)
     pw = models.CharField(max_length=20)
